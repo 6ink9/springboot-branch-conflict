ce59098932ec00b471c1a43d3388fc9e9ea6bb82
“Lorraine“ <3063188782@qq.com>
2024-07-24 21:53:48 +0800

    delete mvnw mvnw.cmd

---
+++ src/main/java/com/example/springbootbranchconflict/SpringbootBranchConflictApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootbranchconflict;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBranchConflictApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBranchConflictApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootbranchconflict/SpringbootBranchConflictApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootbranchconflict;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBranchConflictApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
