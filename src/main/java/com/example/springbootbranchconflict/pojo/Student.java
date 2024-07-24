74edcbcf4315cc4a083bf51d8f70b24323a125bb
“Lorraine“ <3063188782@qq.com>
2024-07-24 22:05:12 +0800

    dev2:1 - 添加Student类:sex

---
+++ src/main/java/com/example/springbootbranchconflict/pojo/Student.java
@@ -0,0 +1,17 @@
+package com.example.springbootbranchconflict.pojo;
+
+import lombok.Data;
+
+/**
+ * Description:
+ * Author:WuXiaotong
+ * Date: 2024/7/24
+ */
+@Data
+public class Student {
+    private Integer id;
+
+    private String name;
+
+    private String sex;
+}
