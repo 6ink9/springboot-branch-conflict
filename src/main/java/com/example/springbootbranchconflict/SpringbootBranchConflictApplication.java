ad6ff0e4ca6416317418fa3a300350a14fcc5433
“Lorraine“ <3063188782@qq.com>
2024-07-24 21:54:21 +0800

    delete mvnw mvnw.cmd

--- src/main/java/com/example/springbootbranchconflict/SpringbootBranchConflictApplication.java
+++ src/main/java/com/example/springbootbranchconflict/SpringbootBranchConflictApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbootBranchConflictApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootBranchConflictApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootBranchConflictApplication.class, args);
+    }
 
 }
